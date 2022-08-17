a278ba384f2bf2a36ed830b6d6e9aa1036497624
Mohammad Toosi <m_dd@rogers.com>
2022-08-17 10:17:24 +0430

    feat: reduce lodash size

--- spec/deserializer-spec.ts
+++ spec/deserializer-spec.ts
@@ -1,134 +1,148 @@
-import { matches } from 'lodash';
+import matches from "lodash.matches";
 
-import { deserialize } from '../src/index';
-import { Document } from '../src/interfaces';
+import { deserialize } from "../src/index";
+import { Document } from "../src/interfaces";
 
-describe('deserialize', () => {
-
-  it('should deserialize jsonapi spec example', () => {
+describe("deserialize", () => {
+  it("should deserialize jsonapi spec example", () => {
     let example: Document = {
-      'data': [{
-        'type': 'articles',
-        'id': '1',
-        'attributes': {
-          'title': 'JSON API paints my bikeshed!'
-        },
-        'links': {
-          'self': 'http://example.com/articles/1'
-        },
-        'relationships': {
-          'author': {
-            'links': {
-              'self': 'http://example.com/articles/1/relationships/author',
-              'related': 'http://example.com/articles/1/author'
-            },
-            'data': { 'type': 'people', 'id': '9' }
+      data: [
+        {
+          type: "articles",
+          id: "1",
+          attributes: {
+            title: "JSON API paints my bikeshed!",
           },
-          'comments': {
-            'links': {
-              'self': 'http://example.com/articles/1/relationships/comments',
-              'related': 'http://example.com/articles/1/comments'
+          links: {
+            self: "http://example.com/articles/1",
+          },
+          relationships: {
+            author: {
+              links: {
+                self: "http://example.com/articles/1/relationships/author",
+                related: "http://example.com/articles/1/author",
+              },
+              data: { type: "people", id: "9" },
             },
-            'data': [
-              { 'type': 'comments', 'id': '5' },
-              { 'type': 'comments', 'id': '12' }
-            ]
-          }
-        }
-      }],
-      'included': [{
-        'type': 'people',
-        'id': '9',
-        'attributes': {
-          'first-name': 'Dan',
-          'last-name': 'Gebhardt',
-          'twitter': 'dgeb'
+            comments: {
+              links: {
+                self: "http://example.com/articles/1/relationships/comments",
+                related: "http://example.com/articles/1/comments",
+              },
+              data: [
+                { type: "comments", id: "5" },
+                { type: "comments", id: "12" },
+              ],
+            },
+          },
         },
-        'links': {
-          'self': 'http://example.com/people/9'
-        }
-      }, {
-        'type': 'comments',
-        'id': '5',
-        'attributes': {
-          'body': 'First!'
+      ],
+      included: [
+        {
+          type: "people",
+          id: "9",
+          attributes: {
+            "first-name": "Dan",
+            "last-name": "Gebhardt",
+            twitter: "dgeb",
+          },
+          links: {
+            self: "http://example.com/people/9",
+          },
         },
-        'relationships': {
-          'author': {
-            'data': { 'type': 'people', 'id': '2' }
-          }
+        {
+          type: "comments",
+          id: "5",
+          attributes: {
+            body: "First!",
+          },
+          relationships: {
+            author: {
+              data: { type: "people", id: "2" },
+            },
+          },
+          links: {
+            self: "http://example.com/comments/5",
+          },
         },
-        'links': {
-          'self': 'http://example.com/comments/5'
-        }
-      }, {
-        'type': 'comments',
-        'id': '12',
-        'attributes': {
-          'body': 'I like XML better'
+        {
+          type: "comments",
+          id: "12",
+          attributes: {
+            body: "I like XML better",
+          },
+          relationships: {
+            author: {
+              data: { type: "people", id: "9" },
+            },
+          },
+          links: {
+            self: "http://example.com/comments/12",
+          },
         },
-        'relationships': {
-          'author': {
-            'data': { 'type': 'people', 'id': '9' }
-          }
-        },
-        'links': {
-          'self': 'http://example.com/comments/12'
-        }
-      }]
+      ],
     };
 
     let result: any = deserialize(example);
 
     let expected = [
       {
-        'id': '1',
-        'title': 'JSON API paints my bikeshed!',
-        'author': {
-          'id': '9',
-          'first-name': 'Dan',
-          'last-name': 'Gebhardt',
-          'twitter': 'dgeb'
+        id: "1",
+        title: "JSON API paints my bikeshed!",
+        author: {
+          id: "9",
+          "first-name": "Dan",
+          "last-name": "Gebhardt",
+          twitter: "dgeb",
         },
-        'comments': [
-          { 'id': '5', 'body': 'First!' },
-          { 'id': '12', 'body': 'I like XML better' }
-        ]
-      }
+        comments: [
+          { id: "5", body: "First!" },
+          { id: "12", body: "I like XML better" },
+        ],
+      },
     ];
 
     expect(matches(expected)(result)).toBe(true);
   });
 
-  it('should deserialize nested resources', () => {
+  it("should deserialize nested resources", () => {
     let document: Document = {
       data: [
         {
-          id: '1', type: 'people', attributes: { name: 'John' },
-          relationships: { friend: { data: { id: '2', type: 'people' } } }
+          id: "1",
+          type: "people",
+          attributes: { name: "John" },
+          relationships: { friend: { data: { id: "2", type: "people" } } },
         },
         {
-          id: '2', type: 'people', attributes: { name: 'Carl' },
-          relationships: { dog: { data: { id: '3', type: 'dogs'} } }
-        }
+          id: "2",
+          type: "people",
+          attributes: { name: "Carl" },
+          relationships: { dog: { data: { id: "3", type: "dogs" } } },
+        },
       ],
       included: [
         {
-          id: '2', type: 'people', attributes: { name: 'Carl' },
-          relationships: { dog: { data: { id: '3', type: 'dogs'} } }
+          id: "2",
+          type: "people",
+          attributes: { name: "Carl" },
+          relationships: { dog: { data: { id: "3", type: "dogs" } } },
         },
-        { id: '3', type: 'dogs', attributes: { name: 'Bobby' } }
-      ]
+        { id: "3", type: "dogs", attributes: { name: "Bobby" } },
+      ],
     };
 
     let result: any = deserialize(document);
 
     let expected = [
-      { id: '1', name: 'John', friend: { id: '2', name: 'Carl', dog: { id: '3', name: 'Bobby' } } },
-      { id: '2', name: 'Carl', dog: { id: '3', name: 'Bobby' } }
+      {
+        id: "1",
+        name: "John",
+        friend: { id: "2", name: "Carl", dog: { id: "3", name: "Bobby" } },
+      },
+      { id: "2", name: "Carl", dog: { id: "3", name: "Bobby" } },
     ];
 
     expect(matches(expected)(result)).toBe(true);
   });
-
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { assign, isArray, mapValues, merge } from 'lodash';
+import assign from "lodash.assign";
+import isArray from "lodash.isarray";
+import mapValues from "lodash.mapvalues";
+import merge from "lodash.merge";
 
 import {
-  Document, Multiple, Serialization,
-  Resource, ResourceId, RelationshipObj, RelationsHash
-} from './interfaces';
+  Document,
+  Multiple,
+  Serialization,
+  Resource,
+  ResourceId,
+  RelationshipObj,
+  RelationsHash,
+} from "./interfaces";
 
 export function deserialize(doc: Document): Serialization {
   return denormalize(doc.data, createRelationshipsHash(doc));
@@ -22,38 +30,51 @@ function createRelationshipsHash(doc: Document): RelationsHash {
   return result;
 }
 
-function denormalize(resources: Multiple<Resource>, relations: RelationsHash): any {
-  return fmapMultiple(resources, (resource: Resource): Serialization =>
-    processResource(resource, relations)
+function denormalize(
+  resources: Multiple<Resource>,
+  relations: RelationsHash
+): any {
+  return fmapMultiple(
+    resources,
+    (resource: Resource): Serialization => processResource(resource, relations)
   );
 }
 
 function processResource(res: Resource, relHash: RelationsHash): Serialization {
   let { id, attributes, relationships } = res;
 
-  let rels: Multiple<any> = mapValues(relationships, (rel: RelationshipObj): any =>
-    fmapMultiple(rel.data, (relId: ResourceId): any => {
-      let relRes = findRelation(relId, relHash);
-      return relRes && processResource(relRes, relHash);
-    })
+  let rels: Multiple<any> = mapValues(
+    relationships,
+    (rel: RelationshipObj): any =>
+      fmapMultiple(rel.data, (relId: ResourceId): any => {
+        let relRes = findRelation(relId, relHash);
+        return relRes && processResource(relRes, relHash);
+      })
   );
 
   return assign({}, attributes, { id }, rels);
 }
 
-function findRelation(rel: ResourceId, relations: RelationsHash): Resource | undefined {
+function findRelation(
+  rel: ResourceId,
+  relations: RelationsHash
+): Resource | undefined {
   return relations[rel.type] && relations[rel.type][rel.id];
 }
 
 // Helper functions
 
-function fmapMultiple<T, R>(data: Multiple<T>, transform: (data: T) => R): Multiple<R> {
+function fmapMultiple<T, R>(
+  data: Multiple<T>,
+  transform: (data: T) => R
+): Multiple<R> {
   // Null Case
-  if (!data) { return undefined;
-  // Array Case
+  if (!data) {
+    return undefined;
+    // Array Case
   } else if (isArray(data)) {
     return data.map(transform);
-  // Single Case
+    // Single Case
   } else {
     return transform(data);
   }
